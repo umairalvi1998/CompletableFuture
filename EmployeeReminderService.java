0afc54857a577a8b129c6801c1b6286926649a46
Umair <umairalvi1998@gmail.com>
2024-10-21 18:46:03 +0530

    Implemented CompletableFuture with practical example

--- EmployeeReminderService.java
+++ EmployeeReminderService.java
@@ -8,37 +8,51 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 public class EmployeeReminderService {
     ObjectMapper mapper = new ObjectMapper();
 
-    public void SendReminderToEmployee() {
-        CompletableFuture.supplyAsync(() -> {
+    public CompletableFuture<Void> SendReminderToEmployee() {
+    CompletableFuture<Void> voidCompletableFuture =
+            CompletableFuture.supplyAsync(() -> {
             try {
-                System.out.println("Thread Name: " + Thread.currentThread().getName());
+                System.out.println("Fetching Employee Stage Thread Name: " + Thread.currentThread().getName());
                 return mapper.readValue(new File("C:\\Users\\Welcome\\Downloads\\employees.json"), new TypeReference<List<Employee>>() {});
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        }).thenApply(employees -> {
-            System.out.println("Thread Name: " + Thread.currentThread().getName());
+        }).thenApplyAsync(employees -> {
+            System.out.println("Filtering Employees Stage Thread Name: " + Thread.currentThread().getName());
             return employees.stream()
                     .filter(employee -> "TRUE".equals(employee.getNewJoiner()))
                     .collect(Collectors.toList()); // Fixed syntax and added missing import
-        }).thenApply( (employees)->{
+        }).thenApplyAsync( (employees)->{
             System.out.println("Thread Name for employees whose training is pending: " + Thread.currentThread().getName());
            return employees.stream()
                    .filter(employee -> "TRUE".equals(employee.getLearningPending()))
                    .collect(Collectors.toList());
-        }).thenApply( (employees) -> {
-            System.out.println("Thread Name for emails: " + Thread.currentThread().getName());
+        }).thenApplyAsync( (employees) -> {
+            System.out.println("Fetching emails Stage Thread Name for emails: " + Thread.currentThread().getName());
             return employees.stream().map(employee -> employee.getEmail()).collect(Collectors.toList());
-        })
+        }).thenAccept((emails)->
+                emails.forEach(this::sendEmailTo)
+        );
+
+      return  voidCompletableFuture;
     }
 
+     public void sendEmailTo(String email) {
+        System.out.println("Sending Training reminder Email to Employee : " + email);
+     }
 
     public static void main(String[] args) {
-
+       EmployeeReminderService employeeReminderService = new EmployeeReminderService();
+        try {
+            employeeReminderService.SendReminderToEmployee().get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
